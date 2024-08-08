2417a149bbab0971b849dd7ab3105e127b3ddc49
Shahd-Ayman5 <148650720+Shahd-Ayman5@users.noreply.github.com>
2024-08-08 09:07:27 +0300

    Add files via upload

---
+++ src/main/java/com/example/oooh/HelloApplication.java
@@ -0,0 +1,35 @@
+package com.example.oooh;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public class TimeTracker {
+
+        public static LocalDateTime getCurrentTime() {
+            return LocalDateTime.now();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
